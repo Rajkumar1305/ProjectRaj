e431b19d6ff1f03703fe4e763bf7501c6ae8013f
Rajkumar1305 <rajkumargsvel@gmail.com>
2022-09-07 16:36:52 +0530

    surya new code

--- src/test/java/org/test/Samples.java
+++ src/test/java/org/test/Samples.java
@@ -3,6 +3,8 @@ package org.test;
 public class Samples {
 
 	public static void main(String[] args) {
-		
+		System.out.println("Test1");
+		System.out.println("Test4");
+		System.out.println("Test5");
 	}
 }
